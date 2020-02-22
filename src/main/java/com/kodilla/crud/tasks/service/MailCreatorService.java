764c6402064c7b1d348e6108a8361c597bcca9c7
Demeqrosad <demetriusz@kurosad.pl>
2020-02-22 21:22:43 +0100

    Module 24.2 20200222

--- src/main/java/com/kodilla/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/kodilla/crud/tasks/service/MailCreatorService.java
@@ -3,6 +3,7 @@ package com.kodilla.crud.tasks.service;
 import com.kodilla.crud.tasks.config.AdminConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
@@ -17,6 +18,13 @@ public class MailCreatorService
     @Qualifier("templateEngine")
     private TemplateEngine templateEngine;
 
+    @Value("${info.app.name}")
+    private String appName;
+
+    @Value("${info.company.name}")
+    private String companyName;
+
+
     public String buildTrelloCardEmail(String message)
     {
         Context context = new Context();
@@ -24,6 +32,9 @@ public class MailCreatorService
         context.setVariable("tasks_url", "http://localhost:8888/tasks_frontend");
         context.setVariable("button", "Visit website");
         context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("appName", appName);
+        context.setVariable("companyName", companyName);
+        context.setVariable("preview_message", "New Trello card was created");
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 }
